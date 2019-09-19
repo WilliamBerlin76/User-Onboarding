28c6cb73526b8719aef162ded10986ec07d9f24a
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-19 18:41:40 -0500

    mvp complete with final stretch for matched email

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const FormDetails = ({errors, touched}) => {
-  
+const FormDetails = ({ values, errors, touched, status }) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status]);
+        }
+    }, [status]);
     return(
         <div className="form-container">
             <h1>Good 'ol Form</h1>
@@ -15,12 +20,19 @@ const FormDetails = ({errors, touched}) => {
                 <Field type="password" name="password" placeholder="Password"/>
                 {touched.password && errors.password && <p>{errors.password}</p>}
                 Agree to Terms of service
-                <Field type="checkbox" name="termsOfService"/>
+                <Field type="checkbox" name="termsOfService" checked={values.termsOfService}/>
                 <button type="submit">Submit</button>
             </Form>
+           {users.map(user => (
+               <ul key={user.id}>
+                   <li>name: {user.name}</li>
+                   <li>email: {user.email}</li>
+                   <li>password: Confidential hehe</li>
+               </ul>
+           ))}
         </div>
-    )
-}
+    );
+};
 const formikFormDetails = withFormik({
     mapPropsToValues({ name, email, password, termsOfService}){
         return{
@@ -39,15 +51,16 @@ const formikFormDetails = withFormik({
                     .min(5, "Minimum 5 characters")
                     .required("This field is required"),
     }),
-  ////////////////handleSubmit below/////////////
-    handleSubmit(values, {resetForm, setErrors, setSubmitting}){
-        if (values.email === "taken@email.com"){
+  ////////////////handleSubmit below with POST request/////////////
+    handleSubmit(values, {resetForm, setErrors, setSubmitting, setStatus}){
+        if (values.email === "waffle@syrup.com"){
             setErrors({email: 'email already taken'});
         } else {
             axios
             .post('https://reqres.in/api/users', values)
             .then(res => {
                 console.log(res);
+                setStatus(res.data)
                 resetForm();
                 setSubmitting(false);
             })
