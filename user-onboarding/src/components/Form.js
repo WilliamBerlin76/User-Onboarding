39ba48b1f0d6f056077f9364f58085e4735f5f5c
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-19 16:56:13 -0500

    validation and error messaging complete

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-
-const FormDetails = () => {
-
+const FormDetails = ({errors, touched}) => {
+  
     return(
         <div className="form-container">
+            <h1>Good 'ol Form</h1>
             <Form>
                 <Field type="text" name="name" placeholder="Name"/>
-                <Field type="text" name="email" placeholder="Email"/>
-                <Field type="text" name="password" placeholder="Password"/>
+                <Field type="email" name="email" placeholder="Email"/>
+                {touched.email && errors.email && <p>{errors.email}</p>}
+                <Field type="password" name="password" placeholder="Password"/>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                Agree to Terms of service
                 <Field type="checkbox" name="termsOfService"/>
                 <button type="submit">Submit</button>
             </Form>
-
         </div>
-
     )
-    
 }
 const formikFormDetails = withFormik({
     mapPropsToValues({ name, email, password, termsOfService}){
@@ -25,9 +26,24 @@ const formikFormDetails = withFormik({
             name: name || '',
             email: email || '',
             password: password || '',
-            termsOfService: termsOfService || ''
+            termsOfService: termsOfService || false
         }
+    },
+  /////////////////Validation Schema below///////////
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+                .email("Must have a standard email format. ex: rand@omuser.com")
+                .required("This field is required"),
+        password: Yup.string()
+                    .min(5, "Minimum 5 characters")
+                    .required("This field is required"),
+    }),
+  ////////////////handleSubmit below/////////////
+    handleSubmit(values){
+        console.log(values);
+        //Form Submition code will go here
     }
+
 })(FormDetails)
 
 export default formikFormDetails
